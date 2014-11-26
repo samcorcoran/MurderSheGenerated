51d076f615fadffbd363dd5aea4ff76634e66714
Sam Corcoran <samuel.corcoran@gmail.com>
2014-11-26 01:39:32 +0000

    Added professional and social relationships into the mix

--- main.py
+++ main.py
@@ -24,8 +24,26 @@ print("Family parameters: number" + str(numFamilies) + ", size" + str(numFamilyM
 # GENERATE ROMANTIC RELATIONSHIP NETWORK
 numRomances = int(0.5 * totalCharacters)
 
+# GENERATE PROFESSIONAL RELATIONSHIP NETWORK
+numEmployers = (int(totalCharacters/6), int(totalCharacters/3))
+numEmployees = (max(2, int(totalCharacters/6)), int(totalCharacters/3))
+if (numEmployers[1] * numEmployees[1] > totalCharacters):
+    print("WARNING: May have too few characters for max possible professional relationships")
+print("Professional parameters: number" + str(numEmployers) + ", size" + str(numEmployees))
+
+# GENERATE SOCIAL RELATIONSHIP NETWORK
+numSocialGroups = (int(totalCharacters/6), int(totalCharacters/3))
+numSocialites = (max(2, int(totalCharacters/6)), int(totalCharacters/3))
+if (numSocialGroups[1] * numSocialites[1] > totalCharacters):
+    print("WARNING: May have too few characters for max possible social relationships")
+print("Social parameters: number" + str(numSocialGroups) + ", size" + str(numSocialites))
+
 c.generateRelationshipGroupings(rType.familial, 1, numFamilies, numFamilyMembers, ConnectionStrategy.totallyConnect)
-#c.generateRelationshipGroupings(rType.romantic, -1, (numRomances, numRomances), (2,2), ConnectionStrategy.totallyConnect)
+c.generateRelationshipGroupings(rType.romantic, -1, (numRomances, numRomances), (2,2), ConnectionStrategy.totallyConnect)
+c.generateRelationshipGroupings(rType.professional, 3, numEmployers, numEmployees, ConnectionStrategy.randomlyConnect)
+c.generateRelationshipGroupings(rType.social, 3, numSocialGroups, numSocialites, ConnectionStrategy.randomlyConnect)
+# Generate non-plot families (wip)
+#c.generateRelationshipGroupings(rType.familial, 1, (totalCharacters,totalCharacters), (1,1), ConnectionStrategy.totallyConnect)
 
 c.createRelationshipEntities()
 
